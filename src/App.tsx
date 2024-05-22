2e36492a658a5dad9d4f65ca4131fb84841d030d
nickramella <41968214+nickramella@users.noreply.github.com>
2024-05-21 17:05:56 -0700

    App Building

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './Components/Header.tsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-full items-center'>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/Components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='text-center bg-red-500 text-white mb-1'>PokeApp</div>
+  )
+}
+
+export default Header;
